8766da41f710768455f6049bf1b8547df4e3b07e
nabilmuafa <109450293+nabilmuafa@users.noreply.github.com>
2025-06-23 22:27:13 +0700

    chore: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,41 @@ import SocialLink from "./components/links/social-link"
 
 function App() {
   return (
-    <main className="flex flex-col justify-center items-center h-screen">
-      <div className="flex flex-col gap-4 font-roboto-mono font-bold text-6xl text-center">
-        <p>🏗️ UNDER</p>
-        <p>DEVELOPMENT!</p>
-        <div className="font-normal">
-          <div className="flex flex-col gap-2 font-normal text-lg">
-            <p>let me cook vro... 🥀</p>
-            <p className="pt-16">...check these in the meantime!</p>
-          </div>
-          <div className="flex justify-center text-base gap-8 mt-2">
-            <SocialLink
-              href="https://linkedin.com/in/nabilmuafa"
-              Icon={FaLinkedin}
-              label="in/nabilmuafa"
-              ariaLabel="LinkedIn Profile"
-            />
-            <SocialLink
-              href="https://instagram.com/nabilmuafa"
-              Icon={FaInstagram}
-              label="nabilmuafa"
-              ariaLabel="Instagram Profile"
-            />
-            <SocialLink
-              href="https://github.com/nabilmuafa"
-              Icon={FaGithub}
-              label="nabilmuafa"
-              ariaLabel="GitHub Profile"
-            />
+    <main className="flex flex-col justify-between items-center h-screen font-roboto-mono">
+      <div className="flex items-center flex-1">
+        <div className="flex flex-col gap-4 font-bold text-6xl text-center">
+          <p>🏗️ UNDER</p>
+          <p>DEVELOPMENT!</p>
+          <div className="font-normal">
+            <div className="flex flex-col gap-2 text-lg">
+              <p>let me cook vro... 🥀</p>
+              <p className="pt-16">...check these in the meantime!</p>
+            </div>
+            <div className="flex justify-center text-base gap-8 mt-2">
+              <SocialLink
+                href="https://linkedin.com/in/nabilmuafa"
+                Icon={FaLinkedin}
+                label="in/nabilmuafa"
+                ariaLabel="LinkedIn Profile"
+              />
+              <SocialLink
+                href="https://instagram.com/nabilmuafa"
+                Icon={FaInstagram}
+                label="nabilmuafa"
+                ariaLabel="Instagram Profile"
+              />
+              <SocialLink
+                href="https://github.com/nabilmuafa"
+                Icon={FaGithub}
+                label="nabilmuafa"
+                ariaLabel="GitHub Profile"
+              />
+            </div>
           </div>
         </div>
+      </div>
+      <div>
+        <p className="text-sm mb-4">nabilmuafa.com</p>
       </div>
     </main>
   )
