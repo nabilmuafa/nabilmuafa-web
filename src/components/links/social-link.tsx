43b88f97ee55071bbaf837c5205ef9342e6fb673
nabilmuafa <109450293+nabilmuafa@users.noreply.github.com>
2025-06-24 11:01:57 +0700

    chore: add & run prettier

--- src/components/links/social-link.tsx
+++ src/components/links/social-link.tsx
@@ -1,22 +1,28 @@
 interface SocialLinkProps {
-	href: string,
-	Icon: React.ElementType,
-	label: string,
-	ariaLabel: string,
+  href: string;
+  Icon: React.ElementType;
+  label: string;
+  ariaLabel: string;
 }
 
-const SocialLink = ({ href, Icon, label, ariaLabel }: SocialLinkProps): React.JSX.Element => {
-	return (
-		<a
-			className="flex items-center hover:underline gap-1"
-			href={href}
-			target="_blank"
-			rel="noopener noreferrer"
-			aria-label={ariaLabel}
-		>
-			<Icon aria-hidden="true" />
-			<span>{label}</span>
-		</a>);
+const SocialLink = ({
+  href,
+  Icon,
+  label,
+  ariaLabel,
+}: SocialLinkProps): React.JSX.Element => {
+  return (
+    <a
+      className="flex items-center hover:underline gap-1"
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label={ariaLabel}
+    >
+      <Icon aria-hidden="true" />
+      <span>{label}</span>
+    </a>
+  );
 };
 
 export default SocialLink;
